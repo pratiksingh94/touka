a873186f85c5b0c9798f8e67bc36a80129ae8d85
pratiksingh94 <pratik.personal4@gmail.com>
2026-03-26 02:23:23 +0530

    fixed some damn path problems that my idiot ass couldnt see

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -2,8 +2,8 @@ export function Links() {
   return (
     <ul className="text-sm text-text-secondary space-y-2">
       <li><a href="https://github.com/pratiksingh94/toka" className="text-accent hover:underline">GitHub</a></li>
-      <li><a href="https://github.com/pratiksingh94/toka/blob/main/DEVLOG.md" className="text-accent hover:underline">Dev Logs</a></li>
-      <li><a href="https://github.com/pratiksingh94/toka/blob/main/CHANGELOG.md" className="text-accent hover:underline">Changelogs</a></li>
+      <li><a href="https://github.com/pratiksingh94/toka/blob/master/DEVLOG.md" className="text-accent hover:underline">Dev Logs</a></li>
+      <li><a href="https://github.com/pratiksingh94/toka/blob/master/CHANGELOG.md" className="text-accent hover:underline">Changelogs</a></li>
       <li><a href="https://github.com/pratiksingh94/toka#resources-used" className="text-accent hover:underline">Resources used</a></li>
       <li><a href="https://flavortown.hackclub.com/projects/18263" className="text-accent hover:underline">Flavortown</a></li>
     </ul>
